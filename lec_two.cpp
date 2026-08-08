9f10124c01f254805642ea9572f95900edfa697e
siya mailik <siyamalik3008@gmail.com>
2026-08-08 12:00:11 +0530

    Initial commit

---
+++ lec_two.cpp
@@ -0,0 +1,12 @@
+//calculator
+#include <iostream>
+using namespace std;
+int main() {
+   int a=108;
+   int b=7;
+   cout << (a%b) << endl;//=>3
+   cout << (a/(double)b) << endl;//=>15.4
+   cout << (a<b)<< endl;//=>0
+   cout << (a*b) << endl;
+    return 0;
+}
