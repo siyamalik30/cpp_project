8c9d44c972232cec3db6886cb9e03662ec98b190
siya mailik <siyamalik3008@gmail.com>
2026-08-14 19:08:37 +0530

    pattern qs added

---
+++ butterfly_pattern.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+int main(){
+   int n=4;
+   for(int i=0;i<n;i++){
+      for(int j=0;j<i+1;j++){
+         cout<<"*";
+      }
+
+      for(int j=0;j<2*(n-i)-2;j++){
+         cout<<" ";
+      }
+      for(int j=0;j<i+1;j++){ 
+         cout<<"*";
+      }
+      cout<<endl;
+   }
+   
+   for(int i=n-1;i>=0;i--){
+      for(int j=0;j<i+1;j++){
+         cout<<"*";
+      }
+      for(int j=0;j<2*(n-i)-2;j++ ){
+         cout<<" ";
+      }
+      for(int j=0;j<i+1;j++){
+         cout<<"*";
+      }
+      cout <<endl;
+   }
+   return 0;
+}
